9dae9c219387f535f2f997cf40ee7c83362b750d
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2025-06-21 22:51:52 +0100

    Time: 0 ms (100%), Space: 19.1 MB (10.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)-1):
-            for x in range(len(nums[i+1:])):
-                if (nums[i] + nums[1+i+x]) == target:
-                    return [i,1+i+x]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i, num in enumerate(nums):
+            if num in map:
+                return [map[num], i]
+            x = target - num
+            map[x] = i
 
-            
-            
-            
-                
+
+        
