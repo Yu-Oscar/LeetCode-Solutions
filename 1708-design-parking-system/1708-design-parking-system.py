03bde2b5b4b4e83eb96a946278d741e6521c066f
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2025-06-21 23:03:03 +0100

    Time: 15 ms (53.6%), Space: 18.4 MB (28.4%) - LeetHub

--- 1708-design-parking-system/1708-design-parking-system.py
+++ 1708-design-parking-system/1708-design-parking-system.py
@@ -1,7 +1,7 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.space = [0, big, medium, small]
+        self.space = {1:big, 2:medium, 3:small}
         
 
     def addCar(self, carType: int) -> bool:
