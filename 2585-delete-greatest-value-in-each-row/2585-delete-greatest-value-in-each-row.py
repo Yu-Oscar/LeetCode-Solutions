6d0b2aba587e5e4ebfd228e6bdeafbe1b8d36633
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2025-06-25 17:54:34 +0100

    Time: 43 ms (26.53%), Space: 12.4 MB (94.9%) - LeetHub

--- 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.py
+++ 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.py
@@ -1,3 +1,26 @@
-class Solution:
-    def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+class Solution(object):
+    def deleteGreatestValue(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        import heapq
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                grid[i][j] = -grid[i][j]
+
+        for row in grid:
+            heapq.heapify(row)
+
+        ans = 0
+        for _ in range(len(grid[0])):
+            max_val = float('-inf')
+            for row in grid:
+                val = -heapq.heappop(row)
+                max_val = max(max_val, val)
+            ans += max_val
+
+        return ans
+
         
