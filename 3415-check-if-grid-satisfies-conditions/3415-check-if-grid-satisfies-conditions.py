56b1b15c280e43e40e6d840f5068d506f9293de0
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2025-06-29 19:13:35 +0100

    Time: 3 ms (38.63%), Space: 17.8 MB (53.3%) - LeetHub

---
+++ 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.py
@@ -0,0 +1,15 @@
+class Solution:
+    def satisfiesConditions(self, grid: List[List[int]]) -> bool:
+        height = len(grid)
+        width = len(grid[0])
+        for i in range(height):
+            for j in range(width):
+                if i != height-1:
+                    if grid[i][j] != grid[i + 1][j]:
+                        return False
+
+                if j != width-1:
+                    if grid[i][j] == grid[i][j + 1]:
+                        return False
+
+        return True 
