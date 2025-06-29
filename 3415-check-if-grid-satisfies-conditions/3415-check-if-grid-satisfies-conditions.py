9e1569d4e06c85095b21bc0e2fd0a188f9b6cb45
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2025-06-29 19:14:54 +0100

    Time: 1 ms (51.83%), Space: 17.6 MB (96.58%) - LeetHub

--- 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.py
+++ 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.py
@@ -4,12 +4,10 @@ class Solution:
         width = len(grid[0])
         for i in range(height):
             for j in range(width):
-                if i != height-1:
-                    if grid[i][j] != grid[i + 1][j]:
-                        return False
+                if i != height-1 and grid[i][j] != grid[i + 1][j]:
+                    return False
 
-                if j != width-1:
-                    if grid[i][j] == grid[i][j + 1]:
-                        return False
+                if j != width-1 and grid[i][j] == grid[i][j + 1]:
+                    return False
 
         return True 
