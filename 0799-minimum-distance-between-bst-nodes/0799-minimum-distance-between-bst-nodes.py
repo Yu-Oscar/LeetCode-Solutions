4dce11dc2e48efbc498ee75d3f74b3dc89d26ff4
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2025-09-17 18:17:35 +0100

    Time: 0 ms (100%), Space: 17.7 MB (70.06%) - LeetHub

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.py
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.py
@@ -12,13 +12,13 @@ class Solution:
         def dfs(node):
             if not node:
                 return
+            
             dfs(node.left)
-
             if self.prev is not None:
                 self.min_diff = min(self.min_diff, node.val - self.prev)
             self.prev = node.val
-
             dfs(node.right)
-
+        
         dfs(root)
         return self.min_diff
+            
