bfcd7e0079665e2edf340011ace527eddcee147c
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2025-10-12 20:19:31 +0100

    Time: 0 ms (100%), Space: 17.6 MB (96.38%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.py
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -1,9 +1,10 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
+        s1, s2, s3 = set("qwertyuiop"), set("asdfghjkl"), set("zxcvbnm")
         ans = []
         for word in words:
             w = set(list(word.lower()))
-            if w <= set("qwertyuiop") or w <= set("asdfghjkl") or w <= set("zxcvbnm"):
+            if w <= s1 or w <= s2 or w <= s3:
                 ans.append(word)
 
         return ans
