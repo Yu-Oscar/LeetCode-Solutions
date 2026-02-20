a716291a11f813cd6d2f25bdef0cc449b30e0c98
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-02-20 00:20:27 +0000

    Time: 11 ms (36.56%), Space: 70.4 MB (61.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,9 +1,6 @@
 function productExceptSelf(nums: number[]): number[] {
-    let ans: number[] = []
-    for (const num of nums) {
-        ans.push(1)
-    }
-
+    let ans = Array(nums.length).fill(1)
+    
     let left: number = 1
     for (let i=0; i<nums.length; i++) {
         ans[i] = left
@@ -15,5 +12,6 @@ function productExceptSelf(nums: number[]): number[] {
         ans[i] *= right
         right *= nums[i]
     }
+    
     return ans
 };
