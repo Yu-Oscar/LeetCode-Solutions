2c5e8e3f7dcca70a41d9359b25caf620b928cc2c
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-02-27 00:15:10 +0000

    Time: 0 ms (100%), Space: 56.1 MB (24.75%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
@@ -2,7 +2,7 @@ function searchMatrix(matrix: number[][], target: number): boolean {
     for (const row of matrix) {
         if (row[0] <= target && target <= row[row.length-1]) {
             let left = 0
-            let right = row.length
+            let right = row.length -1
 
             while (left <= right) {
                 let mid = Math.floor((left+right)/2)
