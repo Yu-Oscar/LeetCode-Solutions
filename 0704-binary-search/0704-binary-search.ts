3907446c7c84e0e2efd1293aaf3fdeb59f8e313e
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-02-27 00:15:16 +0000

    Time: 0 ms (100%), Space: 59 MB (26.49%) - LeetHub

--- 0704-binary-search/0704-binary-search.ts
+++ 0704-binary-search/0704-binary-search.ts
@@ -1,6 +1,6 @@
 function search(nums: number[], target: number): number {
     let left = 0;
-    let right = nums.length
+    let right = nums.length -1
 
     while (left <= right) {
         let mid = Math.floor((left + right)/2)
