c993cc8f62daa0745a7b5d476bd025db874a35dc
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-02-27 00:13:00 +0000

    Time: 0 ms (100%), Space: 55.7 MB (52.13%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
@@ -0,0 +1,18 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+    for (const row of matrix) {
+        if (row[0] <= target && target <= row[row.length-1]) {
+            let left = 0
+            let right = row.length
+
+            while (left <= right) {
+                let mid = Math.floor((left+right)/2)
+
+                if (row[mid] == target) return true
+                if (row[mid] < target) left = mid + 1
+                else right = mid - 1
+            }
+        } 
+    }
+
+    return false
+};
