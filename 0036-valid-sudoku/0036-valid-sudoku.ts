d71293bec17c125d0b06dda3310281d8c9b3b57c
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-03-02 17:55:24 +0000

    Time: 8 ms (30.17%), Space: 59 MB (63%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.ts
+++ 0036-valid-sudoku/0036-valid-sudoku.ts
@@ -1,22 +1,25 @@
 function isValidSudoku(board: string[][]): boolean {
-    const row: boolean[][] = Array.from({ length: 9 }, () => Array(9).fill(false));
-    const col: boolean[][] = Array.from({ length: 9 }, () => Array(9).fill(false));
-    const block: boolean[][] = Array.from({ length: 9 }, () => Array(9).fill(false));
+    const row = Array.from({ length: 9 }, () => new Set());
+    const col = Array.from({ length: 9 }, () => new Set());
+    const block = Array.from({ length: 9 }, () => new Set());
 
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board.length; j++) {
-            const curr = board[i][j]
-            if (curr !== '.') {
-                const num = Number(curr) -1;
-                const b = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+    for (let i = 0; i < board.length; i++){
+        for (let j = 0; j < board[0].length; j++){
+            let val = board[i][j]
+            if (val != '.') {
+                let r = i;
+                let c = j;
+                let b = Math.floor(i / 3)*3 + Math.floor(j / 3)
+                if (row[r].has(val) || col[c].has(val) || block[b].has(val)) {
+                    return false 
+                }
 
-                if (row[i][num] || col[j][num] || block[b][num]) return false
-
-                row[i][num] = true
-                col[j][num] = true
-                block[b][num] = true
+                row[r].add(val)
+                col[c].add(val)
+                block[b].add(val)
             }
-        }
+                
+        }   
     }
 
     return true
