6e1863c3be661362615f156c7935b0b27e9f4873
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-03-02 18:26:51 +0000

    Time: 0 ms (100%), Space: 58.8 MB (36.84%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let ans = null
+    let curr = head
+
+    while (curr !== null){
+        let next = curr.next
+        curr.next = ans
+        ans = curr
+        curr = next
+    }
+
+    return ans
+};
