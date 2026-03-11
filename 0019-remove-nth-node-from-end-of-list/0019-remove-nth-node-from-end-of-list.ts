2507a96bceb6f68c2327d23b18468710ed2c6d67
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-03-11 16:16:20 +0000

    Time: 0 ms (100%), Space: 54.6 MB (96.31%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+    const dummy = new ListNode(0, head)
+
+    let front = head
+    let end = head
+    for (let i = 0; i < n; i++) {
+        end = end.next
+    }
+    if (end == null) return head.next
+
+    while (end && end.next) {
+        front = front.next
+        end = end.next
+    }
+
+    front.next = front.next.next
+    return head
+};
