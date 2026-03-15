1a7a9b42e934746cd8d683cd089dc26315e74e6c
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-03-15 15:11:20 +0000

    Time: 1 ms (84.52%), Space: 60.4 MB (59.83%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.ts
@@ -0,0 +1,18 @@
+function subsetsWithDup(nums: number[]): number[][] {
+    nums = nums.sort()
+    let ans = []
+
+    function recurr(idx, curr) {
+        ans.push([...curr])
+
+        for (let i = idx; i < nums.length; i++) {
+            curr.push(nums[i]);
+            recurr(i + 1, curr);
+            let temp = curr.pop();
+            while (temp == nums[i+1] && i < nums.length) i ++
+        }
+        
+    }
+    recurr(0,[])
+    return ans
+};
