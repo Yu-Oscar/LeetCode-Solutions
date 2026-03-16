b1165668403b4c467460f4975e981aa5ecbb2f3c
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-03-16 12:45:02 +0000

    Time: 2 ms (84%), Space: 61.1 MB (50.75%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.ts
+++ 0039-combination-sum/0039-combination-sum.ts
@@ -1,9 +1,9 @@
 function combinationSum(candidates: number[], target: number): number[][] {
     let ans = []
+    candidates.sort((a,b) => a - b)
     function recurr(idx, can, sum) {
         for (let i = idx; i < candidates.length; i++ ) {
             let curr = candidates[i]
-            console.log(idx, can, sum, curr)
             if (sum + curr < target) {
                 can.push(curr)
                 sum += curr
@@ -11,11 +11,11 @@ function combinationSum(candidates: number[], target: number): number[][] {
                 can.pop()
                 sum -= curr
             } else if (sum + curr == target) {
-                
                 can.push(curr)
                 ans.push([...can])
                 can.pop()
-            }
+                i = candidates.length
+            } else break
         }
     }
 
