0badd029e1505d9db79e856753013c429995e190
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-05-02 13:00:30 +0100

    Time: 11 ms (23.62%), Space: 24.9 MB (6.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,5 +6,13 @@ class Solution:
         for num in nums:
             counts[num] = counts.get(num, 0) + 1
 
-        sorted_keys = sorted(counts, key=counts.get, reverse=True)
-        return sorted_keys[:k]
+        buckets = [[] for i in range(len(nums))]
+        for key, value in counts.items():
+            buckets[value-1].append(key)
+
+        ans = []
+        for bucket in reversed(buckets):
+            for item in bucket:
+                ans.append(item)
+                if len(ans) == k:
+                    return ans
