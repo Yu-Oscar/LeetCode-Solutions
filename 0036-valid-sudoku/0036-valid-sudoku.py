7be53ac7b8841f91ac9b22bf6f970e1890974afa
Yu-Oscar <121000017+Yu-Oscar@users.noreply.github.com>
2026-05-02 14:20:44 +0100

    Time: 0 ms (100%), Space: 19.3 MB (74.83%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,21 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
-        col = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
-        block = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
+        row = [set() for _ in range(len(board))]
+        col = [set() for _ in range(len(board))]
+        block = [set() for _ in range(len(board))]
         
 
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == ".":
                     continue
-                idx = int(board[i][j]) -1
+                val = board[i][j]
 
                 b = (i // 3) * 3 + (j // 3)
 
-                if row[i][idx] or col[j][idx] or block[b][idx]:
+                if val in row[i] or val in col[j] or val in block[b]:
                     return False
                 
-                row[i][idx], col[j][idx], block[b][idx] = True, True, True
+                row[i].add(val)
+                col[j].add(val)
+                block[b].add(val)
 
         return True
